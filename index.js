56de753e6aca3cd172164de0583ab07ebd4789fc
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-10 06:23:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ const randomNumber = Math.random()*20;
 
 const randomInteger = Math.floor(randomNumber);
 
-const randomUserID = randomInteger + oldID + 1;
+const randomUserID = randomInteger + newID + 1;
